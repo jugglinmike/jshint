6468264f32b7554a8be9d79ce5eaf80436de3478
Mike Pennisi <mike@mikepennisi.com>
2015-12-25 21:05:55 -0500

    Remove unused (but better) implementation

--- src/token.js
+++ src/token.js
@@ -6,18 +6,6 @@ function Token(options) {
       this[option] = options[option];
     }
   }
-
-  // Array creation is deferred until required as a performance optimization
-  this.ignored = null;
 }
-
-Token.prototype.ignore = function(id) {
-  if (!this.ignored) {
-    this.ignored = [];
-  }
-  if (this.ignored.indexOf(id) === -1) {
-    this.ignored.push(id);
-  }
-};
 
 module.exports = Token;
