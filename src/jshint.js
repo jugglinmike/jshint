ab989451b19135c908f30ca5b49dd133c2ba6216
Mike Pennisi <mike@mikepennisi.com>
2015-12-27 14:13:15 -0500

    Revert parser improvements
    Revert the changes introduced by
    b3d054e0188575892f29d2b13a524970c60fd8ea

--- src/jshint.js
+++ src/jshint.js
@@ -2555,9 +2555,11 @@ var JSHINT = (function() {
     if (!exprs.length) {
       return;
     }
+
+    exprs[exprs.length - 1].parent = true;
+
     if (exprs.length > 1) {
-      ret = Object.create(state.syntax[","]);
-      ret.exprs = exprs;
+      ret = Object.create(state.syntax[","], { exprs: { value: exprs } });
 
       first = exprs[0];
       last = exprs[exprs.length - 1];
@@ -2606,8 +2608,6 @@ var JSHINT = (function() {
       if (!isNecessary) {
         warning("W126", opening);
       }
-
-      ret.paren = true;
     }
 
     return ret;
@@ -3154,14 +3154,10 @@ var JSHINT = (function() {
   // For example: if (a = 1) { ... }
 
   function checkCondAssignment(expr) {
-    var id, paren;
-    if (expr) {
+    var id = expr.id;
+    if (id === ",") {
+      expr = expr.exprs[expr.exprs.length - 1];
       id = expr.id;
-      paren = expr.paren;
-      if (id === "," && (expr = expr.exprs[expr.exprs.length - 1])) {
-        id = expr.id;
-        paren = paren || expr.paren;
-      }
     }
     switch (id) {
     case "=":
@@ -3173,7 +3169,7 @@ var JSHINT = (function() {
     case "|=":
     case "^=":
     case "/=":
-      if (!paren && !state.option.boss) {
+      if (!expr.paren && !state.option.boss) {
         warning("W084");
       }
     }
@@ -4371,7 +4367,7 @@ var JSHINT = (function() {
     if (!state.option.asi)
       nolinebreak(this);
 
-    if (state.tokens.next.id !== ";" && !state.tokens.next.reach &&
+    if (state.tokens.next.id !== ";" &&
         state.tokens.curr.line === startLine(state.tokens.next)) {
       if (!state.funct["(scope)"].funct.hasBreakLabel(v)) {
         warning("W090", state.tokens.next, v);
@@ -4400,7 +4396,7 @@ var JSHINT = (function() {
     if (!state.option.asi)
       nolinebreak(this);
 
-    if (state.tokens.next.id !== ";" && !state.tokens.next.reach) {
+    if (state.tokens.next.id !== ";") {
       if (state.tokens.curr.line === startLine(state.tokens.next)) {
         if (!state.funct["(scope)"].funct.hasBreakLabel(v)) {
           warning("W090", state.tokens.next, v);
