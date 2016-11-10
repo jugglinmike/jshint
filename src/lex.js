9ac0cb1d4b6b7578111fe3aaea617a12b341e31b
Mike Pennisi <mike@mikepennisi.com>
2016-11-09 22:23:13 -0500

    fixup! [[FEAT]] Introduce exponentiation operator

--- src/lex.js
+++ src/lex.js
@@ -341,23 +341,15 @@ Lexer.prototype = {
       };
     }
 
-    // Exponentiation operator
-    if (ch1 === "*" && ch2 === "*") {
-      return {
-        type: Token.Punctuator,
-        value: "**"
-      };
-    }
-
-    // 2-character punctuators: <= >= == != ++ -- << >> && ||
-    // += -= *= %= &= |= ^= /=
-    if (ch1 === ch2 && ("+-<>&|".indexOf(ch1) >= 0)) {
+    // 2-character punctuators: != ++ -- << >> && || **
+    if (ch1 === ch2 && ("+-<>&|*".indexOf(ch1) >= 0)) {
       return {
         type: Token.Punctuator,
         value: ch1 + ch2
       };
     }
 
+    // <= >= += -= *= %= &= |= ^= /=
     if ("<>=!+-*%&|^/".indexOf(ch1) >= 0) {
       if (ch2 === "=") {
         return {
