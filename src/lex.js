1309411e4385f3882f165c3323246a1bca22b8a0
Mike Pennisi <mike@mikepennisi.com>
2017-08-12 14:47:00 -0400

    PARTIAL: Still a long way to go

--- src/lex.js
+++ src/lex.js
@@ -571,7 +571,7 @@ Lexer.prototype = {
       "super", "return", "typeof", "delete",
       "switch", "export", "import", "default",
       "finally", "extends", "function", "continue",
-      "debugger", "instanceof"
+      "debugger", "instanceof", "async"
     ];
 
     if (result && keywords.indexOf(result[0]) >= 0) {
