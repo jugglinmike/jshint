b3f15961260cc8d2f574affd661f43295edc0bff
Mike Pennisi <mike@mikepennisi.com>
2018-02-04 11:51:49 -0500

    Implement naive parallelization

--- tests/test262/index.js
+++ tests/test262/index.js
@@ -8,7 +8,6 @@ var Transform = require("stream").Transform;
 var Test262Stream = require("../../../../test262-stream");
 var Interpreter = require("../../../../results-interpreter");
 
-var runTest = require("./test");
 var report = require("./report");
 var expectationsFile = path.join(__dirname, "expectations.txt");
 var shouldUpdate = process.argv.indexOf("--update-expectations") > -1;
@@ -16,18 +15,42 @@ var stream = new Test262Stream(path.join(__dirname, "test262"), {
   omitRuntime: true
 });
 
+var child_process = require("child_process");
+var os = require("os");
+var workers = [];
+var cpuCount = os.cpus().length;
+var worker, index;
+
+for (index = 0; index < cpuCount; index += 1) {
+  worker = child_process.fork(path.join(__dirname, "worker.js"));
+  worker.on("message", (result) => {
+    dones[result.id](null, result);
+    delete dones[result.id];
+  });
+  workers.push(worker);
+}
+
 function normalizePath(str) {
   return path.posix.format(path.parse(str));
 }
 
+var dones = Object.create(null);
+var testCount = 0;
 var results = new Transform({
   objectMode: true,
   transform(test, encoding, done) {
-    done(null, {
-      id: normalizePath(test.file) + "(" + test.scenario + ")",
-      expected: test.attrs.negative && test.attrs.negative.phase === "early"
-        ? "fail" : "pass",
-      actual: runTest(test) ? "pass": "fail"
+    test.id = normalizePath(test.file) + "(" + test.scenario + ")",
+    testCount += 1;
+    if (testCount % 1000 === 0) {
+      console.log(testCount);
+    }
+
+    dones[test.id] = done;
+
+    workers[testCount % cpuCount].send(test, function(err) {
+      if (err) {
+        done(err);
+      }
     });
   }
 });
@@ -56,6 +79,9 @@ stream.pipe(results)
     process.exitCode = 1;
   })
   .on("finish", function () {
+    workers.forEach(function(worker) {
+      worker.disconnect();
+    });
     report(this.summary);
     process.exitCode = this.summary.passed ? 0 : 1;
   });
